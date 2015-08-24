88742060013b40d8528a63e74c2ef8b124e22b33
Dieferson <diefersonsoares@hotmail.com>
2015-08-24 14:14:10 -0400

    zueira

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+# -*- coding: cp1252 -*-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello World!!!</h1> EU CONSEGUI FAZER DEPLOY "
+    return "<h1>Hello World!!!</h1> <span>Finalmente conseguir fazer Deploy nessa porra õ/ </span>"
 
 if __name__ == "__main__":
     app.run(debug=True, port=33507)
