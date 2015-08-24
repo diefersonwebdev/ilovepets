fdf0f9cc1bc58f2afe4e483d4fefa8183140d993
Dieferson <diefersonrcc@gmail.com>
2015-08-24 20:49:08 -0300

    Modificação Pastas

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-# -*- coding: cp1252 -*-
 from flask import Flask, render_template
+from wtforms import Form, BooleanField, TextField, PasswordField, validators
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+@app.route("/dashboard")
+def dashboard():
+	return render_template("dashboard.html")
+
+
 if __name__ == "__main__":
-    app.run(debug=True, port=33507)
+    app.run(debug=True)
