ba4b7bc14f631e478c7232ef512994bdfa1e9b53
Dieferson <diefersonsoares@hotmail.com>
2015-08-24 16:22:44 -0400

    correção

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 # -*- coding: cp1252 -*-
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello World!!!</h1> <h2>Finalmente conseguir fazer Deploy nessa porra KKK </h2>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=33507)
